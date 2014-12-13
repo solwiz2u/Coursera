ec77b7ca66f1416eaec3c6e21a9057c96f569e90
Paul A. Johnston <mcasspj@outlook.com>
2014-12-13 17:28:07 +0000

    Added inverse calculation

--- cachematrix.r
+++ cachematrix.r
@@ -5,6 +5,24 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+print("Running makeCacheMatrix")
+print("Find the size")
+# create an identity matrix of the same size
+print(x)
+if ( nrow(x) == ncol(x)) {
+print("Square")
+size <- nrow(x)
+line <- c(1,rep(0,size))
+full <- rep(line,size)
+print(line)
+print(full)
+Ident <- matrix(full,nrow=size,ncol=size)
+print(Ident)
+Inv <- solve(x,I)
+
+} else {
+print("Not square")
+}
 
 }
 
